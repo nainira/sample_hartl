748dcdc893aa787a3451b756fe5bceaedd55c3cd
Ryan Kim <nainirajk@gmail.com>
2014-06-14 23:34:10 +0900

    add Signin/Signin upon Signup features and tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,16 +10,18 @@ describe "AuthenticationPages" do
 
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) } 
+
+      
       before do
-        fill_in "Email",  with: "user.email.upcase" 
-        fill_in "Password",  with: "user.password" 
+        visit signin_path
+        fill_in "Email",  with: user.email.upcase
+        fill_in "Password",  with: user.password 
         click_button "Sign in"
       end
-
       it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) } 
-      it { should have_link('Sign out', href: signout_path) } 
-      it { should_not have_link('Sign in', href: signin_path) } 
+      it { should have_link("Profile", href: user_path(user)) } 
+      it { should have_link("Sign out", href: signout_path) } 
+      it { should_not have_link("Sign in", href: signin_path) } 
     end
 
     describe 'with invalid information' do
