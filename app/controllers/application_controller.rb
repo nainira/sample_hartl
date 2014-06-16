35a19643ed9d3535a1a9cd7a6704e6b21dc088ea
Ryan Kim <nainirajk@gmail.com>
2014-06-16 13:46:53 +0900

    add some commented authentication on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # ssl for production
+  # force_ssl
+
+  # simple http basic authentication
+  # http_basic_authenticate_with :name => "test", :password => "password"
+
   include SessionsHelper
 end
