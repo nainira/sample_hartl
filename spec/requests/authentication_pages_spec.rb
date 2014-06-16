bfb1db1a74733572ad8a29c3f720111d6e9d41d7
Ryan Kim <nainirajk@gmail.com>
2014-06-17 01:05:17 +0900

    add user profile edit feature

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,10 +12,12 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) } 
       before do
         visit signin_path
-        valid_signin(user)
+        # valid_signin(user)
+        sign_in user
       end
       it { should have_title(user.name) }
       it { should have_link("Profile", href: user_path(user)) } 
+      it { should have_link('Settings', href: edit_user_path(user)) } 
       it { should have_link("Sign out", href: signout_path) } 
       it { should_not have_link("Sign in", href: signin_path) } 
       describe 'followed by a signout' do
