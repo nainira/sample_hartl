4c79c7e350b0c82dedaa2d1813a6ae5c9d02aca1
Ryan Kim <nainirajk@gmail.com>
2014-06-19 21:42:11 +0900

    add user micropost and apply the right order on them

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should be_valid }
   it { should respond_to(:authenticate) } 
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) } 
   
   it { should be_valid }
   it { should_not be_admin }
@@ -123,4 +124,17 @@ describe User do
     end
   end
 
+  describe 'micropost associations' do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end # micropost associations should have the right microposts in the right order
+  end # micropost associations
 end # User Test
