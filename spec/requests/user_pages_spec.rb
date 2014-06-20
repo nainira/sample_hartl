3880157c422c68a3138f9fad928ecb2cbd2c88b9
Ryan Kim <nainirajk@gmail.com>
2014-06-20 10:28:41 +0900

    hot fix for testing profile micropost

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -60,8 +60,8 @@ describe "UserPages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) } 
-    let(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") } 
-    let(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") } 
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") } 
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") } 
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
@@ -69,7 +69,7 @@ describe "UserPages" do
 
     describe 'microposts' do
       it { should have_content(m1.content) }
-      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
     end
   end # profile page
