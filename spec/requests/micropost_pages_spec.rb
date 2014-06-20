93c3cd5c498e28688fdcca71751947e06d433a01
Ryan Kim <nainirajk@gmail.com>
2014-06-21 01:47:28 +0900

    add delete feature for microposts in home and profile

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -27,4 +27,16 @@ describe "MicropostPages" do
       end
     end # with valid info
   end # micropost creation
-end
+
+  describe 'micropost destruction' do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe 'as correct user' do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end # micropost destruction
+end # Micropost Page
