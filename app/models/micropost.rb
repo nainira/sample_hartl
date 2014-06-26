ffe004c84296fcb37496f5ce2e521a81896cf67b
Ryan Kim <nainirajk@gmail.com>
2014-06-26 16:15:53 +0900

    add better feeds query for scailing

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,14 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    # followed_user_ids = user.followed_user_ids
+    # where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    
+    # better query for big data
+    followed_user_ids = "SELECT FOLLOWED_ID
+						FROM RELATIONSHIPS
+						WHERE FOLLOWER_ID = :user_id"
+    where("USER_ID IN (#{followed_user_ids}) OR USER_ID = :user_id",
+          user_id: user)
   end
 end
